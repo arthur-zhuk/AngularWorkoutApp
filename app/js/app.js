b537a525d7cb5067e15afe7a1409b5b551b1fc71
Arthy <arthurzhuk@gmail.com>
2015-10-31 20:49:32 +0000

    late on the init

---
+++ app/js/app.js
@@ -0,0 +1,18 @@
+angular.module('app', ['ngRoute', 'ngSanitize', '7minWorkout']).
+config(function($routeProvider, $sceDelegateProvider) {
+    $routeProvider.when('/start', {
+        templateUrl: 'partials/start.html'
+    });
+    $routeProvider.when('/workout', {
+        templateUrl: 'partials/workout.html',
+        controller: 'WorkoutController'
+    });
+    $routeProvider.when('/finish', {
+        templateUrl: 'partials/finish.html'
+    });
+    $routeProvider.otherwise({
+        redirectTo: '/start'
+    });
+    $sceDelegateProvider.resourceUrlWhitelist(['self', 'http://*.youtube.com/**']);
+});
+angular.module('7minWorkout', []);
